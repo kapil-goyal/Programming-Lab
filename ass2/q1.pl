40989383a36d8725e68a9a2093cd104909824e83
kapil-goyal <kapil.goyal61@gmail.com>
2019-10-26 22:59:43 +0530

    Assignment 2 Ques 1 commented

--- ass2/q1.pl
+++ ass2/q1.pl
@@ -1,15 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate is used to check if the character is an Alphabet or not.
+% Params:
+%    Num : Ascii Value of character
+%    Res : True if it is an alphabet
+%                else fails
+% 
+% Ascii value is greater than 'Z'
+
 is_Alpha(Num, Res) :-
     Num > 90,
     Res is 0.
+
+% Ascii value is less than 'A'
 
 is_Alpha(Num, Res) :-
     Num < 65,
     Res is 0.
 
+% Ascii Value lies in between 'A' to 'Z'
+
 is_Alpha(Num, Res) :-
     Num >= 65,
     Num =< 90,
     Res is 1.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This Predicate concatenates two strings
 
 join_str(Str1, Str2, Str3) :-
     name(Str1, Strlist1),
@@ -17,14 +35,24 @@ join_str(Str1, Str2, Str3) :-
     append(Strlist1, Strlist2, Strlist3),
     name(Str3, Strlist3).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate is used in predicate concat_str_in_list_str
+
+% No element left in list of strings
+
 my_loop(_, 0, _, Res) :-
     Res = [].
+
+% Single element left in list of strings
 
 my_loop(X, 1, Str, Res) :-
     [First_ele | []] = X,
     join_str(Str, First_ele, StrRes),
     % write(StrRes), nl,
     Res = [StrRes].
+
+% Recursive loop to iterate in each element in list of strings
 
 my_loop(X, _, Str, Res) :-
     [First_ele | Res_list] = X,
@@ -34,18 +62,36 @@ my_loop(X, _, Str, Res) :-
     my_loop(Res_list, Length, Str, Res2),
     Res = [StrRes | Res2].
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate calls my_loop and join string in list of strings
+% params :-
+%       X : List of strings
+%       Str : String to be concatenated
+%       Res: To store the resultant list
+
 concat_str_in_list_str(X, Str, Res) :-
     length(X, Length),
     my_loop(X, Length, Str, Res).
     % write(Res).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate is used to make recursion calls on the given string
+
+% Base Case 1: When complete string is used 
+
 dp(_, 0, Res) :- 
     Res = [''].
+
+% Base Case 2: When first character of string is '0'
 
 dp(Strlist, _, Res) :-
     [X|_] = Strlist,
     X = 48,
     Res = [].
+
+% Base Case 3: When First character is not zero and only one character is left
 
 dp(Strlist, 1, Res) :-
     [X|_] = Strlist,
@@ -53,6 +99,12 @@ dp(Strlist, 1, Res) :-
     W is (16 + X),
     name(Char, [W]),
     Res = [Char].
+
+% Main Recursion: 
+%       Get first character
+%       Call Recursion for remaining string
+%       If first two characters can make an alphabet then call dp_util
+%       Concatenate first character with the Result of remaining string
 
 dp(Strlist, _, Res) :-
     [X, Y | Z] = Strlist,
@@ -67,14 +119,20 @@ dp(Strlist, _, Res) :-
     name(FirstChar, [W]),
     concat_str_in_list_str(Res3, FirstChar, Res4),
     append(Res2, Res4, Res).
-    
+
+% Base Case 1: No string left  
+
 dp_util(_, 0, Res) :-
     Res = [].
+
+% Base Case 2: It is possible to make a character from first two letters and first character is '0'
 
 dp_util(Strlist, 1, Res) :-
     [X|_] = Strlist,
     X = 48,
     Res = [].
+
+% Main Recursion: It is possible to make a character from first two letters and first character is not '0'
 
 dp_util(Strlist, 1, Res) :-
     [X, Y | RemStrList] = Strlist,
@@ -84,6 +142,11 @@ dp_util(Strlist, 1, Res) :-
     dp(RemStrList, Length, Res3),
     name(FirstChar, [Temp]),
     concat_str_in_list_str(Res3, FirstChar, Res).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate is the main function of the program
+% X is given input string of numbers
 
 decode(X) :-
     name(X, StrList),
@@ -96,9 +159,18 @@ decode(X) :-
     format('Possible strings: ~n'),
     write(Res).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate checks if the given character is number or not
+
 is_num(Char) :-
     Char >= 48,
     Char =< 57.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate checks if the length of the input string is positive or not.
+% If not print error message.
 
 check_length(StrList) :-
     length(StrList, Length),
@@ -107,6 +179,11 @@ check_length(StrList) :-
 check_length(_) :-
     format('Please enter valid input. ~n'),
     fail.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% This predicate checks that thed given input only contains numbers.
+% If not then print error message
 
 check_invalid_input([]).
 
@@ -120,4 +197,5 @@ check_invalid_input(StrList) :-
     \+ is_num(Head),
     format('Please enter valid input. ~n'),
     fail.
-    
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
